11c04e0630e2b6d1d1fca7ca98e2100c5412cf60
demonxdrag <adrian_xdrag@hotmail.com>
2016-09-30 01:26:02 +0200

    starting working session

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
-/*window.onload = function home_circle(){
-  var id = setInterval(frame, 10);
-  var circle = document.getElementById('home_circle')
-  var x = 0.0
-  function startup_frame() {
-    if (circle.style.boxShadow == '0px 10px 50px rgba(41,41,41,1)') {
-        clearInterval(id);
-    } else {
-        x = x + 0.1
-        circle.style.boxShadow = "0px 10px 50px rgba(41,41,41," + x + ")"
-    }
-  }
-};*/
 $(document).ready(function(){
-  console.log("ff");
-  $('#home_circle').css('opacity') = '0';
+  $('#home_circle')
+    .velocity({
+      opacity: "1"
+    }, {
+      duration: 700,
+      easing: "easeInQuad"
+    })
+    .velocity({
+      color: "#414141"
+    }, {
+      duration: 1000
+    });
 });
