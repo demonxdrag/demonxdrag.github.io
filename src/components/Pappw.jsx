98ccd20a1a58654f725ae1d1eda978a1be5bd7f0
Daniel Fernandez Rico <deferrico@gmail.com>
2016-10-04 09:34:30 +0200

    Added timeout, trying to animate stuff

--- src/components/Pappw.jsx
+++ src/components/Pappw.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Bar from './Bar';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Papp extends React.Component {
 	constructor(props){
@@ -10,22 +11,36 @@ class Papp extends React.Component {
 		this.setState(store);
 	}
 	showThing(newState){
-		let state = Object.assign(this.state, newState);
-		this.setState(state);
+		let mergedState = Object.assign(this.state, newState);
+		this.setState(mergedState);
+	}
+	componentDidMount(){
+		document.querySelectorAll('#home_circle');
+		setTimeout(() => this.showThing({circle:false}), 1500);
 	}
 	render() {
 		let asdf;
 		if(this.state.circle){
-			asdf =  <div id='home_circle' onClick={() => this.showThing()}></div>;
+			asdf =  <div id='home_circle'></div>;
 		}
 
 		let a = <div>
 			{asdf}
-			<Bar headerText='Potato1' yes={true}></Bar>;
-			<Bar headerText={1} yes={true}></Bar>;
-			<Bar headerText={3} yes={true}></Bar>;
+			<Bar headerText='Potato1' yes={true}></Bar>
+			<Bar headerText={1} yes={true}></Bar>
+			<Bar headerText={3} yes={true}></Bar>
 		</div>;
-		return a;
+		let transitionCircle = <ReactCSSTransitionGroup 
+          transitionName="circle" 
+          transitionEnterTimeout={500} 
+          transitionLeaveTimeout={300}
+					
+					transitionAppear={true} 
+					transitionAppearTimeout={500}>
+					{a}
+    </ReactCSSTransitionGroup>;
+		
+		return transitionCircle;
 	}
 }
 
